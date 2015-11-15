66b422648ef1eedfdcd70c48e913ede229da1342
Rodrigo Sarmiento <rs@manoderecha.mx>
2015-11-14 23:34:28 -0600

    Fix nginx setup to complain with wordpress workflow

--- virtualhost-nginx.sh
+++ virtualhost-nginx.sh
@@ -96,10 +96,13 @@ if [ "$action" == 'create' ]
 			error_page 404 /index.php;
 
 			location ~ \.php$ {
+                try_files \$uri =404;
 				fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-				fastcgi_pass 127.0.0.1:9000;
+				fastcgi_pass unix:/var/run/php5-fpm.sock;
 				fastcgi_index index.php;
+                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
 				include fastcgi_params;
+                fastcgi_read_timeout 3000;
 			}
 
 			location ~ /\.ht {
@@ -123,11 +126,7 @@ if [ "$action" == 'create' ]
 				echo -e $"Host added to /etc/hosts file \n"
 		fi
 
-		if [ "$owner" == "" ]; then
-			chown -R $(whoami):www-data $userDir$rootDir
-		else
-			chown -R $owner:www-data $userDir$rootDir
-		fi
+		chown -R vagrant:vagrant $userDir$rootDir
 
 		### enable website
 		ln -s $sitesAvailable$domain $sitesEnable$domain
